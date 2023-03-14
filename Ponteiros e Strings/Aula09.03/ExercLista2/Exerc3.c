52dc4e08f26aadfecfc168db8a40ec78eea1491d
Erico Bis Dos Santos <21201069@facin.portoalegre.pucrsnet.br>
2023-03-14 10:06:10 -0300

    change - Exerc3 da Lista2

--- Ponteiros e Strings/Aula09.03/ExercLista2/Exerc3.c
+++ Ponteiros e Strings/Aula09.03/ExercLista2/Exerc3.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 // minha solução
+
 void reverse(int vet[], int size)
 {
-    int aux;
-    for (int i = 1; i < size; i++)
+    int* p1 = vet;
+    int* p2 = vet+size-1;
+    for (int i = 0; i < size / 2; i++)
     {
-        for (int j = 0; j < i; j++)
-        {
-            aux = *(vet + i);
-            *(vet + i) = *(vet + j);
-            *(vet + j) = aux;
-        }
+        int aux = *p1;
+        *p1 = *p2;
+        *p2 = aux;
+        p1++;
+        p2--;
     }
 }
 
@@ -27,8 +28,25 @@ int main()
 {
     int vet[] = {10, 20, 30, 40, 50, 60};
     int size = 6;
-    
+
     reverse(vet, size);
-    printf("Vetor invertido: ");
+    printf("\nVetor invertido: ");
     printVector(vet, size);
 }
+
+/*
+void reverse(int vet[], int size)
+{
+    int aux;
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            aux = *(vet + i);
+            *(vet + i) = *(vet + j);
+            *(vet + j) = aux;
+        }
+    }
+}
+
+*/
